fc9f0d3fbbfd6dc3e9f89011a95d668997ccb034
James Chen <jchen@cpacketnetworks.com>
2026-04-09 15:42:06 -0700

    update tmux theme

--- nvim/.config/nvim/lua/james/plugins/codediff.lua
+++ nvim/.config/nvim/lua/james/plugins/codediff.lua
@@ -4,6 +4,14 @@ return {
 	cmd = "CodeDiff",
 	opts = {
 		keymaps = {
+			view = {
+				next_hunk = "]h", -- Jump to next change
+				prev_hunk = "[h", -- Jump to previous change
+				toggle_stage = "<leader>hS", -- Stage/unstage current file (works in explorer and diff buffers)
+				stage_hunk = "<leader>hs", -- Stage hunk under cursor to git index
+				unstage_hunk = "<leader>hu", -- Unstage hunk under cursor from git index
+				discard_hunk = "<leader>hr", -- Discard hunk under cursor (working tree only)
+			},
 			conflict = {
 				accept_incoming = "<leader>hct", -- Accept incoming (theirs/left) change
 				accept_current = "<leader>hco", -- Accept current (ours/right) change
@@ -11,8 +19,6 @@ return {
 				discard = "<leader>hcx", -- Discard both, keep base
 				next_conflict = "]x", -- Jump to next conflict
 				prev_conflict = "[x", -- Jump to previous conflict
-				diffget_incoming = "2do", -- Get hunk from incoming (left/theirs) buffer
-				diffget_current = "3do", -- Get hunk from current (right/ours) buffer
 			},
 		},
 	},
