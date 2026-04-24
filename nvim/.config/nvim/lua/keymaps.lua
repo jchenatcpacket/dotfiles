5d1564ea03981d8328f7cc00a9f39e482e43148b
James Chen <jchen@cpacketnetworks.com>
2026-04-24 12:33:20 -0700

    more keymaps

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -12,3 +12,21 @@ vim.keymap.set({ "n", "v" }, "<C-w>2", ":2wincmd c<cr>", { desc = "close window 
 vim.keymap.set({ "n", "v" }, "<C-w>3", ":3wincmd c<cr>", { desc = "close window 3" })
 vim.keymap.set({ "n", "v" }, "<C-w>4", ":4wincmd c<cr>", { desc = "close window 4" })
 vim.keymap.set({ "n", "v" }, "<C-w>5", ":5wincmd c<cr>", { desc = "close window 5" })
+
+vim.keymap.set("n", "wt", function()
+	local count = vim.v.count
+	if count == 0 then
+		vim.cmd("tablast") -- or whatever you want for bare 'wt'
+	else
+		vim.cmd(count .. "tabnext")
+	end
+end, { desc = "Go to tab" })
+
+vim.keymap.set("n", "ww", function()
+	local count = vim.v.count
+	if count == 0 then
+		vim.cmd("wincmd w") -- default: go to next window
+	else
+		vim.cmd(count .. "wincmd w")
+	end
+end, { desc = "Go to window" })
