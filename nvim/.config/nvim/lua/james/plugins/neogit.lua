4287963403eaff4a37ba875e7e2b67662dd28b93
James Chen <jchen@cpacketnetworks.com>
2026-05-01 14:07:16 -0700

    add neogit

---
+++ nvim/.config/nvim/lua/james/plugins/neogit.lua
@@ -0,0 +1,12 @@
+return {
+	"NeogitOrg/neogit",
+	lazy = true,
+	cond = not vim.g.vscode,
+	dependencies = {
+		"esmuellert/codediff.nvim",
+	},
+	cmd = "Neogit",
+	keys = {
+		{ "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" },
+	},
+}
