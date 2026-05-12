6dacd04ee68db91df36d20bf42a0edb99c5c1505
James Chen <jchen@cpacketnetworks.com>
2026-05-12 11:02:28 -0700

    vscode condition updates

--- nvim/.config/nvim/lua/james/plugins/trouble.lua
+++ nvim/.config/nvim/lua/james/plugins/trouble.lua
@@ -1,5 +1,6 @@
 return {
 	"folke/trouble.nvim",
+	cond = not vim.g.vscode,
 	opts = {
 		win = {
 			position = "right",
--- nvim/.config/nvim/lua/james/plugins/wayfinder.lua
+++ nvim/.config/nvim/lua/james/plugins/wayfinder.lua
@@ -1,5 +1,6 @@
 return {
 	"error311/wayfinder.nvim",
+	cond = not vim.g.vscode,
 	config = function()
 		require("wayfinder").setup({})
 		vim.keymap.set("n", "<leader>w", "<Plug>(WayfinderOpen)", { desc = "Wayfinder" })
