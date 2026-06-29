0df1497dcaa7ae049c937ba6a9d19f716b6ed150
James Chen <jchen@cpacketnetworks.com>
2026-06-29 15:43:00 -0700

    set highlight color

--- nvim/.config/nvim/lua/james/plugins/git-conflict.lua
+++ nvim/.config/nvim/lua/james/plugins/git-conflict.lua
@@ -3,6 +3,10 @@ return {
 	version = "*",
 	cond = not vim.g.vscode,
 	config = function()
+		vim.api.nvim_set_hl(0, "GitConflictCurrentLabel", { bg = "#2a5c64" }) -- Current header
+		vim.api.nvim_set_hl(0, "GitConflictCurrent", { bg = "#29444c" }) -- Current body
+		vim.api.nvim_set_hl(0, "GitConflictIncomingLabel", { bg = "#48604a" }) -- Incoming header
+		vim.api.nvim_set_hl(0, "GitConflictIncoming", { bg = "#38463f" }) -- Incoming body
 		require("git-conflict").setup({})
 		vim.keymap.set("n", "<leader>xl", "<Plug>(GitConflictListQf)", { desc = "git conflict list conflicts" })
 		vim.keymap.set("n", "<leader>xc", "<Plug>(git-conflict-ours)", { desc = "git conflict choose current" })
