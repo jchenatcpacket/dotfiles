d3c405d93bd2ef88a72eece37bb96b3008820714
James Chen <jchen@cpacketnetworks.com>
2026-08-11 15:44:45 -0700

    rm markdown

--- nvim/.config/nvim/lua/james/plugins/markdown.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"MeanderingProgrammer/render-markdown.nvim",
-    cond = not vim.g.vscode,
-	opts = {
-		anti_conceal = { enabled = false },
-		file_types = { "markdown", "opencode_output" },
-	},
-	ft = { "markdown", "codecompanion", "copilot-chat", "opencode_output" },
-}
