089813bddaf773fe27feccd68f4936f6e9aaf2df
James Chen <jchen@cpacketnetworks.com>
2026-09-09 09:44:33 -0700

    add mini indent

--- nvim/.config/nvim/lua/james/plugins/mini.lua
+++ nvim/.config/nvim/lua/james/plugins/mini.lua
@@ -23,4 +23,11 @@ return {
 			require("mini.pairs").setup()
 		end,
 	},
+	{
+		"nvim-mini/mini.indentscope",
+		version = "*",
+		config = function()
+			require("mini.indentscope").setup()
+		end,
+	},
 }
