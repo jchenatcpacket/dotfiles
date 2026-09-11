f7a144af2b611541e9b8065e213b4fbaa55d8f81
James Chen <jchen@cpacketnetworks.com>
2026-09-11 13:20:17 -0700

    small configs

--- nvim/.config/nvim/lua/james/plugins/mini.lua
+++ nvim/.config/nvim/lua/james/plugins/mini.lua
@@ -27,7 +27,7 @@ return {
 		"nvim-mini/mini.indentscope",
 		version = "*",
 		config = function()
-			require("mini.indentscope").setup()
+			require("mini.indentscope").setup({ symbol = "│" })
 		end,
 	},
 }
