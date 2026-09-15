532f681a01728a4d452f1541e45fe75138d4415e
James Chen <jchen@cpacketnetworks.com>
2026-09-15 16:38:32 -0700

    update indetn plugin

---
+++ nvim/.config/nvim/lua/james/plugins/blink-indent.lua
@@ -0,0 +1,14 @@
+return {
+	"saghen/blink.indent",
+	--- @module 'blink.indent'
+	--- @type blink.indent.Config
+	opts = {
+		static = {
+			enabled = false,
+			char = "│",
+		},
+		scope = {
+			char = "│",
+		},
+	},
+}
--- nvim/.config/nvim/lua/james/plugins/mini.lua
+++ nvim/.config/nvim/lua/james/plugins/mini.lua
@@ -23,11 +23,4 @@ return {
 			require("mini.pairs").setup()
 		end,
 	},
-	{
-		"nvim-mini/mini.indentscope",
-		version = "*",
-		config = function()
-			require("mini.indentscope").setup({ symbol = "│" })
-		end,
-	},
 }
