c7cda4a5499f3e319d06aaab22a9a25cd3dd49ad
James Chen <jchen@cpacketnetworks.com>
2026-09-17 16:27:43 -0700

    added lazygit

---
+++ nvim/.config/nvim/lua/james/plugins/lazygit.lua
@@ -0,0 +1,14 @@
+return {
+	"kdheepak/lazygit.nvim",
+	lazy = true,
+	cmd = {
+		"LazyGit",
+		"LazyGitConfig",
+		"LazyGitCurrentFile",
+		"LazyGitFilter",
+		"LazyGitFilterCurrentFile",
+	},
+	keys = {
+		{ "<leader>gz", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+	},
+}
